7612cd42624fbd85fe2c0fd9fb7767d969b0ab49
Essouma Lovett <essoumangoelovett@gmail.com>
2023-07-04 06:17:27 +0100

    Setup routing path

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,27 @@
 
 
 <script setup>
-
+import {RouterView} from 'vue-router'
 
 </script>
 
 
 
 
-<template>
-  <h1>Food and Decoration Website</h1>
+<template >
+  <h1 class="main">Food and Decoration Website</h1>
+  <RouterView />
 </template>
 
 
 <style scoped>
+  .main{
+    text-align: center;
+    font-size: 2rem;
+    font-weight: bold;
+    margin-top: 40px;
+
+    
+  }
  
 </style>
